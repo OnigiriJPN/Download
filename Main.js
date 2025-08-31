f4acbbdf1b48468b441ad3b25ea3df81ad2b6576
OnigiriJPN <yuzuhajintian@gmail.com>
2025-08-31 13:28:07 +0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,4 +1,10 @@
 // クリック関数
-function Download(){
-  
+function onSubmit(token) {
+    const fileUrl = "https://onigiriapps1.netlify.app/Assets/ApplicationFiles.zip"; // リポジトリに置いたファイル
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = "Setup.zip";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
