df0563728cc3f85c0a46b6fa7b6ed11651ce777d
OnigiriJPN <yuzuhajintian@gmail.com>
2025-08-31 13:34:34 +0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,10 +1,16 @@
 // クリック関数
 function onSubmit(token) {
-    const fileUrl = "https://onigiriapps1.netlify.app/Assets/ApplicationFiles.zip"; // リポジトリに置いたファイル
-    const a = document.createElement("a");
-    a.href = fileUrl;
-    a.download = "Setup.zip";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    try{
+        if(!token) throw new Error("CAPTCHA認証に失敗");
+        const fileUrl = "https://onigiriapps1.netlify.app/Assets/ApplicationFiles.zip"; // リポジトリに置いたファイル
+        const a = document.createElement("a");
+        a.href = fileUrl;
+        a.download = "Setup.zip";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }catch(error){
+        alert("認証エラー:"+error);
+        throw new Error(error);
+    }
 }
