06566ddc43fc1d6700630771325a48841556036f
OnigiriJPN <yuzuhajintian@gmail.com>
2025-08-31 14:11:19 +0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,19 +1,7 @@
 // クリック関数
 async function onSubmit(token) {
-    window.reportError = function(errorMessage, errorObj = null) {
-        // ユーザー向けアラート
-        alert("エラーが発生しました: " + errorMessage);
-        
-        // デバッグ用コンソール出力
-        console.error(errorMessage, errorObj);
-    }
     try{
         if(!token) throw new Error("CAPTCHA認証に失敗");
-        const response = await fetch("/.netlify/functions/verify", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ token })
-        });
 
         if (!response.ok) throw new Error("CAPTCHA検証に失敗しました");
 
@@ -27,7 +15,7 @@ async function onSubmit(token) {
         a.click();
         document.body.removeChild(a);
     }catch(err){
-        window.reportError(err.message, err);
+        console.log(err);
     }
     
 }
