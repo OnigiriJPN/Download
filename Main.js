fc99bfe71b64090ea96af682756c66fc4b7c9fb2
OnigiriJPN <yuzuhajintian@gmail.com>
2025-09-05 17:23:14 +0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -15,3 +15,12 @@ async function onSubmit(token) {
     }
     
 }
+let ut = navigator.userAgent;
+
+	if(ut.indexOf('iPhone') > 0 || ut.indexOf('iPod') > 0 || ut.indexOf('Android') > 0 && ut.indexOf('Mobile') > 0){
+		msg = "SmartPhon";
+	}else if(ut.indexOf('iPad') > 0 || ut.indexOf('Android') > 0){
+		msg ="Tablet";
+	}else{
+		msg = "Personal Computer";
+	}
